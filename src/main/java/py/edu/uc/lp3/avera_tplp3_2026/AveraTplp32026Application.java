2839526d6fbfe4906a9ed2905775494e63d482ff
Adrian-312 <adrielias2005@gmail.com>
2026-09-25 19:26:00 -0300

    chore:esqueleto Spring Boot API REST desde start.spring.io

---
+++ src/main/java/py/edu/uc/lp3/avera_tplp3_2026/AveraTplp32026Application.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3.avera_tplp3_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AveraTplp32026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AveraTplp32026Application.class, args);
+	}
+
+}
---
+++ src/test/java/py/edu/uc/lp3/avera_tplp3_2026/AveraTplp32026ApplicationTests.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3.avera_tplp3_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AveraTplp32026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
