fa657a63ceebd96c52aac839133916f7ef643eef
javifacha123 <javier.atlasovich@gmail.com>
2026-09-25 19:10:31 -0300

    chore: inicializar proyecto Spring Boot 3

---
+++ src/main/java/py/edu/uc/lp3/ja_taller_git_2026/JaTallerGit2026Application.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3.ja_taller_git_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaTallerGit2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaTallerGit2026Application.class, args);
+	}
+
+}
